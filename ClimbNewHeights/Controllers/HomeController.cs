85d7f9a160f0a23f58d9a1abe953cc4150e9a0e4
ch843 <cassidy.hardisty@gmail.com>
2024-02-09 23:30:26 -0700

    connor saving the day

--- ClimbNewHeights/Controllers/HomeController.cs
+++ ClimbNewHeights/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ClimbNewHeights.Controllers
             return View();
         }
 
+        public IActionResult Book()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
